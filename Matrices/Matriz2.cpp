bffb88e522b986d685323a626a49d5446c2dc942
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-16 10:05:57 -0500

    Arreglar y terminar
    Termine el programa con matrices para jugar triqui pero se debe definir la posicion de las jugadas desde el principio

--- Matrices/Matriz2.cpp
+++ Matrices/Matriz2.cpp
@@ -36,5 +36,6 @@ int main(){
         cout<<endl;
     }
 
+    return 0;
 
 }
--- Matrices/Matriz3.cpp
+++ Matrices/Matriz3.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 int main(){
 
-    string triqui [3][3];
 
-    
+    int filas = 3;
+    int columnas = 3;
+
+    string triqui [filas][columnas] = {
+
+        {"O"," ","X"},
+        {" ","O","X"},
+        {"X"," ","O"}
+
+    };
+
+    cout<<"Estado inicial del triqui"<<endl;
+
+    for (int i = 0; i < filas; i++){
+        for (int j = 0; j < columnas; j++){
+
+            cout<< triqui [i][j]<<" ";
+
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
 
 }
